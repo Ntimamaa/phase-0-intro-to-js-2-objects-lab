f9161715f1e2bb9605ed6ff8e6f605bd130a2990
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-09 15:15:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+
+}
+function updateEmployeeWithKeyAndValue(employee,name,Sam){
+    const newObj = { ...employee };
+
+    newObj[name] = Sam;
+  
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, Broadway) {
+    employee[streetAddress] = Broadway; 
+    return employee; 
+  }
+
+  function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = { ...employee }; 
+    delete newEmployee[name]; 
+    return newEmployee; 
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name];
+    return employee; 
+  }
+  
+  
